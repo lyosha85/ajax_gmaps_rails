19e3373e39218d8a30e01e96d4792edfa82d19c9
a1 <lyosha85@gmail.com>
2013-06-28 15:19:48 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AjaxGmapsRails::Application.config.secret_key_base = 'f8c9bfcdb84d8a2096edc26c048f1cb662b48cba84d261c9b9acf974d9ea801a7958d34bc0f9d79fee523c698fc973274d3a6ad34b53f63066d444dc00520339'
